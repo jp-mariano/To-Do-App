9867d19d1510ecd4223ba45f941f0097164bf7cc
jpmariano <juanpaolo.mariano28@gmail.com>
2021-06-21 19:17:26 +0800

    Added bcryptjs as one of dependencies for password hashing and added routes and controllers for email dupe checking and new user registration.

--- controllers/user.js
+++ controllers/user.js
@@ -4,7 +4,36 @@
 const User = require('../models/User');
 
 // Authentication
-
+const auth = require('../auth');
 
 // bcryptjs
+const bcryptjs = require('bcryptjs');
 
+// To check if user's email is already in the DB
+module.exports.emailExists = async (body) => {
+	try {
+		const user = await User.find({ email: body.email });
+		return user.length > 0 ? true : false;
+		
+	} catch (err) {
+		console.error(err);
+	}
+};
+
+// To register new user
+module.exports.register = async (body) => {
+	try {
+		const user = new User({
+			givenName: body.givenName,
+			familyName: body.familyName,
+			email: body.email,
+			password: bcryptjs.hashSync(body.password, 10)
+		});
+		
+		await user.save(); //When this fails, it goes to catch
+		return true; //If registration is a success, return true
+		
+	} catch (err) {
+		console.error(err)
+	}
+};
--- routes/user.js
+++ routes/user.js
@@ -3,11 +3,29 @@ const express = require('express');
 const router = express.Router();
 
 // Authentication
-
+const auth = require('../auth');
 
 // Controller
 const UserController = require('../controllers/user');
 
 // ROUTES
+
+// To check if user's email is already in the DB
+router.post('/email-exists', async (req, res) => {
+	try {
+		res.send(await UserController.emailExists(req.body));
+	} catch (err) {
+		console.error(err);
+	}
+});
+
+// To register new user
+router.post('/register', async (req, res) => {
+	try {
+		res.send(await UserController.register(req.body));
+	} catch (err) {
+		console.error(err);
+	}
+});
 
 module.exports = router;
